b843f256b7d509689b5ed985edd9a313643d06c5
Aliviessa <125681566+Aliviessa@users.noreply.github.com>
2025-04-15 21:02:12 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -657,7 +657,7 @@ require('lazy').setup({
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
       require('lspconfig').gdscript.setup(capabilities)
       require('lspconfig')['gdscript'].setup {
         name = 'godot',
